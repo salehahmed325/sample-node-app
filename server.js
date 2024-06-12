f688a008de19409f7832512d550af343754d4200
salehahmed325 <salehahmed904@gmail.com>
2024-06-12 15:32:37 +0600

    updated server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Welcome to page Cloud Academy!!</h1> \n');
+  res.send('<h1 style="color:green;">Welcome to kubernetes!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
