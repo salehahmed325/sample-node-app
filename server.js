3145060d54b1b25f5963b469b0c42787a158e235
salehahmed325 <salehahmed904@gmail.com>
2024-06-16 14:17:19 +0600

    updated server.js

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Welcome to kubernetes!!</h1> \n');
-  res.send('<p style="color:gray;">This is a simple node.js app running on kubernetes cluster on a home labe environment.!</p>')
+  res.send('<h1 style="color:green;">Welcome to kubernetes!! This is a simple node.js app running on kubernetes cluster on a home labe environment.</h1> \n');
 });
 
 app.listen(PORT, HOST);
